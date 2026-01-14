25b60939055d325e124e47c96ecd76d6ff074afe
Infinity-MaxX <katerina@oestergaard.com>
2026-01-14 23:02:29 +0100

    Finished L5

--- main.js
+++ main.js
@@ -2,9 +2,18 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
+            description: 'Warm socks for your feet.',
             image: './assets/images/socks_blue.jpg',
+            url: 'https://www.vuemastery.com/courses/intro-to-vue-3/conditional-rendering-vue3',
             inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
+            inventory: 8,
+            onSale: false,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 2234, colour: 'green' },
+                { id: 2235, colour: 'blue' },
+            ],
+            sizes: [36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46], 
         }
     }
 })
