92f41b4bcd03e7850ee7a7cc6007133ada508988
Infinity-MaxX <katerina@oestergaard.com>
2026-01-14 23:07:03 +0100

    Setting stage

--- main.js
+++ main.js
@@ -3,13 +3,18 @@ const app = Vue.createApp({
         return {
             cart:0,
             product: 'Socks',
+            description: 'Warm socks for your feet.',
             image: './assets/images/socks_blue.jpg',
+            url: 'https://www.vuemastery.com/courses/intro-to-vue-3/conditional-rendering-vue3',
             inStock: true,
+            inventory: 8,
+            onSale: false,
             details: ['50% cotton', '30% wool', '20% polyester'],
+            sizes: ['S', 'M', 'L', 'XL'],
             variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
-            ]
+                { id: 2234, colour: 'green', image: './assets/images/socks_green.jpg' },
+                { id: 2235, colour: 'blue', image: './assets/images/socks_blue.jpg' },
+            ],
         }
     }
 })
