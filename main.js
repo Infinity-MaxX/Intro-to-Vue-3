ca59c92f52ba9156de18bc01ecd92f547b4480f0
Infinity-MaxX <katerina@oestergaard.com>
2026-01-15 17:33:53 +0100

    L10 Finished

--- main.js
+++ main.js
@@ -10,9 +10,24 @@ const app = Vue.createApp({
             this.cart.push(id)
         },
         // Code Challenge
+            // splice: 
+                // first parameter is required; it
+                // dictates at what index should an
+                // item be removed. 
+                // second parameter is optional; it
+                // dictates the count, i.e. how many,
+                // items should be removed. 0 is used
+                // when simply wants to add item.
+                // third and above parameters are
+                // optional; here you can write which
+                // items should be added to the array
         deleteItem(id) {
-            this.cart.
-        }
+            const index = this.cart.indexOf(id)
+            // remove from the back of the array
+            if (index > -1) {
+                this.cart.splice(index, 1)
+            }
+        },
         // Code Challenge
     }
 })
